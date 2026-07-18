a38a2a7e5d4ce3592ba79321d374108151e2436b
umeshtharukaoffical <umeshtharukaofficial@gmail.com>
2026-07-18 11:09:49 +0000

    chore: initialize Spring Boot 3 project with base dependencies

---
+++ buildtrack/src/main/java/com/buildtrack/BuildtrackApplication.java
@@ -0,0 +1,13 @@
+package com.buildtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildtrackApplication.class, args);
+	}
+
+}
